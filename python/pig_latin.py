ab4911ff1440970704a552b3fceb511c152aa2e2
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-01 19:56:59 -0400

    Pair programmed with Andrew and John, partially functional. Not handling multi words well.

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,20 @@
 def translate(word_or_phrase):
-  pass
 
+    if len(word_or_phrase.split()) == 1:
+        return translate_word(word_or_phrase)
+    elif len(word_or_phrase.split()) > 1:
+        answer = []
+        for x in word_or_phrase:
+            answer.append(translate_word(x))
+        return " ".join(str(answer))
+
+
+def translate_word(word_or_phrase):
+    key = ['a','e','i','o','u','y']
+    hold = [char for char in word_or_phrase]
+    for x in range(len(word_or_phrase)):
+        if hold[0] in key:
+            return(''.join(hold) + 'ay')
+        else:
+            hold.append((hold.pop(0)))
+    print('no vowels found')
--- python/pig_latin_test.py
+++ python/pig_latin_test.py
@@ -1,17 +1,32 @@
 # Rewrite this in Unit Test
-
+import unittest
 from pig_latin import translate
 
-print(f"translates a word beginning with a vowel: {translate('apple') == 'appleay'}")
-print(f"translates a word beginning with a consonant: {translate('banana') == 'ananabay'}")
-print(f"translates a word beginning with two consonants: {translate('cherry') == 'errychay'}")
-print(f"translates two words: {translate('eat pie') == 'eatay iepay'}")
-print(f"translates a word beginning with three consonants: {translate('three') == 'eethray'}")
-print(f"counts 'sch' as a single phoneme: {translate('school') == 'oolschay'}")
-print(f"counts 'qu' as a single phoneme: {translate('quiet') == 'ietquay'}")
-print(f"counts 'qu' as a consonant even when it's preceded by a consonant: {translate('square') == 'aresquay'}")
-print(f"translates many words: {translate('the quick brown fox') == 'ethay ickquay ownbray oxfay'}")
+class PigTest(unittest.TestCase):
 
-# write a test asserting that capitalized words are still capitalized
-# (but with a different initial capital letter, of course) retain the
-# punctuation from the original phrase
+    def test_returns_a_string(self):
+        self.assertTrue(type(translate("a")) == type("str"))
+
+    def test_translates_leading_vowel(self):
+        self.assertEqual(translate('apple'), 'appleay')
+
+    def test_translates_beginning_consonant(self):
+        self.assertTrue(translate('banana') == 'ananabay')
+    
+    def test_translates_beginning_two_consonants(self): 
+        self.assertTrue(translate('cherry') == 'errychay')
+    
+    def test_translates_two_words(self):
+        self.assertTrue(translate('eat pie') == 'eatay iepay')
+# print(f"translates a word beginning with three consonants: {translate('three') == 'eethray'}")
+# print(f"counts 'sch' as a single phoneme: {translate('school') == 'oolschay'}")
+# print(f"counts 'qu' as a single phoneme: {translate('quiet') == 'ietquay'}")
+# print(f"counts 'qu' as a consonant even when it's preceded by a consonant: {translate('square') == 'aresquay'}")
+# print(f"translates many words: {translate('the quick brown fox') == 'ethay ickquay ownbray oxfay'}")
+
+# # write a test asserting that capitalized words are still capitalized
+# # (but with a different initial capital letter, of course) retain the
+# # punctuation from the original phrase
+
+if __name__ == '__main__':
+    unittest.main()
