69a088dd66837290acc8c021fc2e2c0a1853f205
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-01 19:02:37 -0400

    temp

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,28 @@
 def translate(word_or_phrase):
-  pass
+  #split by word into array
+  #iterate through word/phrase
+  #check if first word is constant
+  #ignore space
+  #place constant at end, then add ay to end
+  #vowel just add ay at end
+  #return string
 
+  split = list(word_or_phrase.split(' '))
+  #pushing list items in array
+  list_holding = []
+  #from list_holding convert back to string
+  final = ''
+  for word in split:
+    word = word[0]
+
+    #Still looking for regex to check for vowel/would compare upper/lower seperately
+    #Y is not a real vowel
+    if word in ('a','e','i','o','u'):
+      word = f'{word}ay' 
+      list_holding += word 
+    elif word in ('A','E','I','O','U'):
+      word = f'{word}ay'
+      list_holding += word 
+  return print(list_holding)
+
+translate('test a test O A')
