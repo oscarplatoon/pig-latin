069e965608f4cc5d3170ec35c8e47c7aaa4caa9f
jakewhite5 <jakewhite5@yahoo.com>
2021-06-02 07:38:38 -0700

    basic logic written, still need to implement parsing sentences and words that start with vowels.

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,16 @@
 def translate(word_or_phrase):
-  pass
+  return_word = ''
+  vowel_list = ['a', 'e', 'i', 'o', 'u']
+  for letter in word_or_phrase:
+    if letter in vowel_list:
+      return_word += word_or_phrase[(word_or_phrase.index(letter))::]
+      return_word += word_or_phrase[:(word_or_phrase.index(letter))]
+      return_word += 'ay'
+      print(return_word)
+
+
+
+translate('zach')
+
+
 
