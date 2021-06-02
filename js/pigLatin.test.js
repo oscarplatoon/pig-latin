419c0ed1cfef92237dae51c5730e3681ddc8efd2
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-02 09:17:02 -0400

    All tests implemented.

--- js/pigLatin.test.js
+++ js/pigLatin.test.js
@@ -4,33 +4,51 @@ const{ test } = require ('@jest/globals')
 
 
 test('Does it return a string?', () => {
-  expect(typeof(pig.translate("test"))).toBe('string')
+  expect(typeof(pig.translate("test")))
+  .toBe('string')
 });
 
 test("translates a word beginning with a vowel:", () => {
-  expect((pig.translate("apple"))).toBe("appleay");
+  expect((pig.translate("apple")))
+  .toBe("appleay");
 });
 
 test("translates a word beginning with a consonant:", () => {
-  expect(pig.translate("banana")).toBe("ananabay")
+  expect(pig.translate("banana"))
+  .toBe("ananabay")
 });
 
 test("translates a word beginning with two consonants:", () => {
-  expect(pig.translate("cherry")).toBe("errychay")
+  expect(pig.translate("cherry"))
+  .toBe("errychay")
 })
 
 test("translates two words:", () => {
-  expect(pig.translate("eat pie")).toBe("eatay iepay")
+  expect(pig.translate("eat pie"))
+  .toBe("eatay iepay")
 })
 
 test("translates a word beginning with three consonants:", () => {
-  expect(pig.translate("three")).toBe("eethray")
+  expect(pig.translate("three"))
+  .toBe("eethray")
 });
 
 test("counts 'sch' as a single phoneme:", () => {
-  expect(pig.translate("school")).toBe("oolschay")
+  expect(pig.translate("school"))
+  .toBe("oolschay")
 });
 
 test("counts 'qu' as a single phoneme:", () => {
-  expect(pig.translate("quiet")).toBe("ietquay")
-}); 
+  expect(pig.translate("quiet"))
+  .toBe("ietquay")
+});
+
+test("counts 'qu' as a consonant even when it's preceded by a consonant:", () => {
+  expect(pig.translate("square"))
+  .toBe("aresquay");
+});
+
+test("translates many words:", () => {
+  expect(pig.translate("the quick brown fox"))
+  .toBe("ethay ickquay ownbray oxfay");
+});
