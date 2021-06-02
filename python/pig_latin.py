dc469692b718d032581fd47298a67cb0210e608e
jakewhite5 <jakewhite5@yahoo.com>
2021-06-02 09:33:40 -0700

    python solution mostly done except for casing issues

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,16 +1,25 @@
 def translate(word_or_phrase):
-  return_word = ''
+  return_phrase = ''
   vowel_list = ['a', 'e', 'i', 'o', 'u']
-  for letter in word_or_phrase:
-    if letter in vowel_list:
-      return_word += word_or_phrase[(word_or_phrase.index(letter))::]
-      return_word += word_or_phrase[:(word_or_phrase.index(letter))]
-      return_word += 'ay'
-      print(return_word)
+  word_list = word_or_phrase.split(' ')
+  for word in word_list:
+    for letter in word:
+      return_phrase += ' '
+      if letter in vowel_list:
+        if word.index(letter) == 0:
+          return_phrase += word
+          return_phrase += 'ay'
+          break
+        else:
+          return_phrase += word[(word.index(letter))::]
+          return_phrase += word[:(word.index(letter))]
+          return_phrase += 'ay'
+          break
+  print(return_phrase.strip())
 
 
 
-translate('zach')
+translate('Zach Attack')
 
 
 
