eda7bd0e634746ef135ad4f9eb74fbf4ea0f1adf
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-01 20:14:17 -0400

    solution

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -24,11 +24,13 @@ def translate(word_or_phrase):
     elif first_letter_of_word in ('A','E','I','O','U'):
       whole_word_or_phrase = f'{whole_word_or_phrase}ay'
       list_holding.append(whole_word_or_phrase)
+    #If letter is constant
     elif first_letter_of_word not in ('a','e','i','o','u','A','E','I','O','U'):
       constant_hold = whole_word_or_phrase[1:] + whole_word_or_phrase[0]
       whole_word_or_phrase = f'{constant_hold}ay'
       list_holding.append(whole_word_or_phrase)
       
+  #Join and return seperated by space
   final = ' '.join(list_holding)
   return print(final)
 
