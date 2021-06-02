3579b682e28526e67e02978e38afa3fc3e1eeab4
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-02 08:21:44 -0500

    Shawn's partial solution to pig latin in python

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,51 @@
+# Input is a regular english word or phrase
+# Separate input into an list of individual words
+# the individual words include punctuation
+# pass individual words to function pig latin to
+# convert
+# output is a combined string of pig latin words
 def translate(word_or_phrase):
-  pass
+  word_list = word_or_phrase.split(" ")
+  output_list = []
 
+  for elem in word_list:
+    output_list.append(pig_latin(elem))
+    
+  return " ".join(output_list)
+
+
+
+# Input is a regular english word
+# create a variable converted_word to store result
+# if the word starts with vowel, add -ay to word
+# and save to converted_word
+# if the word starts with a consonant, move all consonants
+# up to the first vowel to end end and save to converted_word
+# add -ay to the end of converted_word
+# return converted_word
+
+
+def pig_latin(word):
+
+  vowel = ['a', 'e', 'i', 'o', 'u']
+  converted_word = ""
+
+  if (word[0].lower() in vowel):
+      converted_word = word + "ay"
+  
+  else:
+    word_array = list(word)
+    
+    while (not word_array[0].lower() in vowel):
+      new_end_ch = word_array.pop(0)
+      print (new_end_ch)
+      word_array.append(new_end_ch)
+    
+    
+    converted_word = "".join(word_array) + "ay"
+
+
+  return converted_word
+
+
+print(translate("I'm"))
