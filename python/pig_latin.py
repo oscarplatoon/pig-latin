8c4c8b5327307b53170eec033000adb2450a4fce
Marc Stanley <marcson909@gmail.com>
2021-06-01 23:19:21 -0500

    unfinished solution. passes simple cases. need to refactor to solve edge cases and correctly handle multiple word strings

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,16 @@
 def translate(word_or_phrase):
-  pass
+  vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+  phrase = word_or_phrase
+  if word_or_phrase[0] in vowels:
+    phrase = word_or_phrase + 'ay'
+
+    return phrase
+  else:
+    phrase_list = []
+    phrase_list[:] = phrase
+    phrase_list.append(phrase[0])
+    phrase_list.pop(0)
+    phrase_list.append('ay')
+
+    return ''.join(phrase_list)
 
--- python/pig_latin_test.py
+++ python/pig_latin_test.py
@@ -1,16 +1,31 @@
 # Rewrite this in Unit Test
-
+import unittest
 from pig_latin import translate
 
-print(f"translates a word beginning with a vowel: {translate('apple') == 'appleay'}")
-print(f"translates a word beginning with a consonant: {translate('banana') == 'ananabay'}")
-print(f"translates a word beginning with two consonants: {translate('cherry') == 'errychay'}")
-print(f"translates two words: {translate('eat pie') == 'eatay iepay'}")
-print(f"translates a word beginning with three consonants: {translate('three') == 'eethray'}")
-print(f"counts 'sch' as a single phoneme: {translate('school') == 'oolschay'}")
-print(f"counts 'qu' as a single phoneme: {translate('quiet') == 'ietquay'}")
-print(f"counts 'qu' as a consonant even when it's preceded by a consonant: {translate('square') == 'aresquay'}")
-print(f"translates many words: {translate('the quick brown fox') == 'ethay ickquay ownbray oxfay'}")
+class PigLatinTestCase(unittest.TestCase):
+    
+    def test_apple(self):
+        self.assertEqual(translate('apple'), 'appleay')
+    
+    def test_banana(self):
+        self.assertEqual(translate('banana'), 'ananabay')
+    
+    def test_cherry(self):
+        self.assertEqual(translate('cherry'), 'errychay')
+    
+    def test_two(self):
+        self.assertEqual(translate('eat pie'), 'eatay iepay')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# print(f"translates two words: {translate('eat pie') == 'eatay iepay'}")
+# print(f"translates a word beginning with three consonants: {translate('three') == 'eethray'}")
+# print(f"counts 'sch' as a single phoneme: {translate('school') == 'oolschay'}")
+# print(f"counts 'qu' as a single phoneme: {translate('quiet') == 'ietquay'}")
+# print(f"counts 'qu' as a consonant even when it's preceded by a consonant: {translate('square') == 'aresquay'}")
+# print(f"translates many words: {translate('the quick brown fox') == 'ethay ickquay ownbray oxfay'}")
 
 # write a test asserting that capitalized words are still capitalized
 # (but with a different initial capital letter, of course) retain the
