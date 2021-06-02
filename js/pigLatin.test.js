76f76b04fc616b9d1c11b64205ce381ddb5e506d
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-02 07:59:10 -0400

    Implemented more tests

--- js/pigLatin.test.js
+++ js/pigLatin.test.js
@@ -5,9 +5,32 @@ const{ test } = require ('@jest/globals')
 
 test('Does it return a string?', () => {
   expect(typeof(pig.translate("test"))).toBe('string')
+});
 
 test("translates a word beginning with a vowel:", () => {
   expect((pig.translate("apple"))).toBe("appleay");
-
-
 });
+
+test("translates a word beginning with a consonant:", () => {
+  expect(pig.translate("banana")).toBe("ananabay")
+});
+
+test("translates a word beginning with two consonants:", () => {
+  expect(pig.translate("cherry")).toBe("errychay")
+})
+
+test("translates two words:", () => {
+  expect(pig.translate("eat pie")).toBe("eatay iepay")
+})
+
+test("translates a word beginning with three consonants:", () => {
+  expect(pig.translate("three")).toBe("eethray")
+});
+
+test("counts 'sch' as a single phoneme:", () => {
+  expect(pig.translate("school")).toBe("oolschay")
+});
+
+test("counts 'qu' as a single phoneme:", () => {
+  expect(pig.translate("quiet")).toBe("ietquay")
+}); 
--- js/pigLatinSpec.js
+++ js/pigLatinSpec.js
@@ -2,18 +2,18 @@
 
 var pig = require("./pigLatin");
 
-console.log("translates a word beginning with a vowel:");
-console.log(pig.translate("apple") === "appleay");
-console.log("translates a word beginning with a consonant:");
-console.log(pig.translate("banana") === "ananabay");
-console.log("translates a word beginning with two consonants:");
-console.log(pig.translate("cherry") === "errychay");
-console.log("translates two words:");
-console.log(pig.translate("eat pie") === "eatay iepay");
-console.log("translates a word beginning with three consonants:");
-console.log(pig.translate("three") === "eethray");
-console.log("counts 'sch' as a single phoneme:");
-console.log(pig.translate("school") === "oolschay");
+// console.log("translates a word beginning with a vowel:");
+// console.log(pig.translate("apple") === "appleay");
+// console.log("translates a word beginning with a consonant:");
+// console.log(pig.translate("banana") === "ananabay");
+// console.log("translates a word beginning with two consonants:");
+// console.log(pig.translate("cherry") === "errychay");
+// console.log("translates two words:");
+// console.log(pig.translate("eat pie") === "eatay iepay");
+// console.log("translates a word beginning with three consonants:");
+// console.log(pig.translate("three") === "eethray");
+// console.log("counts 'sch' as a single phoneme:");
+// console.log(pig.translate("school") === "oolschay");
 console.log("counts 'qu' as a single phoneme:");
 console.log(pig.translate("quiet") === "ietquay");
 console.log("counts 'qu' as a consonant even when it's preceded by a consonant:");
